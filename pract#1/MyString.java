7cbf1417667981ec8e2cb77891e6af3066d037df
Евгений Киселев <jkiselev@siroca.com>
2018-10-05 11:53:13 +0600

    add tasks

---
+++ pract#1/MyString.java
@@ -0,0 +1,21 @@
+class MyString{
+
+	public static void main(String[] args){
+
+	}
+
+
+	}
+
+	public static void show(String msg){
+		System.out.println("==========");
+		System.out.println(msg);
+		System.out.println("==========");
+
+	public static void getch(){
+		try{
+			System.in.read();
+		}catch (Exception e){}		
+	}
+	
+}
