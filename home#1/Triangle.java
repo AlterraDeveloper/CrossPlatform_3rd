13c87bfe6b8d013ff43e3fefa4813dbe8ea4194f
Evgenii Kiselev <jkiselev2411@gmail.com>
2018-10-08 00:41:54 +0800

    home#1 done

--- home#1/Triangle.java
+++ home#1/Triangle.java
@@ -1,38 +1,68 @@
 import java.util.*;
+import java.lang.Math;
 
-public class Triangle{
-	public static void main(String[] args){
+public class Triangle
+{
+
+double hypotenuse,catet1,catet2;
+double[] angles;
+double perimeter;
+double square;
+
+public Triangle(double catet1,double catet2)
+{
+	this.catet1 = catet1;
+	this.catet2 = catet2;
+	hypotenuse = calculateHypotenuse();
+	angles = new double[3];
+	angles[0] = 90.0;
+	angles[1] = calculateAngle();
+	angles[2] = 90.0 - angles[1];
+	perimeter = calculatePerimeter();
+	square = calculateSquare();
+}
+
+private double calculateHypotenuse()
+{
+	return 	Math.sqrt(Math.pow(catet1,2)+Math.pow(catet2,2));
+}
+
+private double calculateAngle()
+{
+	return (Math.acos(catet1/hypotenuse))*(180.0d/Math.PI);
+}
+
+private double calculatePerimeter()
+{
+	return hypotenuse+catet1+catet2;
+}
+
+private double calculateSquare(){
+	return 0.5*catet1*catet2;
+}
+
+public void info()
+{
+	System.out.printf("Hypotenuse : %.2f\nFirst catet : %.2f\nSecond catet : %.2f\n",
+					hypotenuse,catet1,catet2);
+	System.out.printf("Angles: %.2f %.2f %.2f\n",angles[0],angles[1], angles[2]);
+	System.out.printf("Square: %.2f\n",square);
+	System.out.printf("Perimeter: %.2f\n",perimeter);
+}
+
+public static void main(String[] args)
+{
 
 		if(args.length == 0 || args.length>2){
 		System.out.println("Incoorect args!\nExample: java Triangle 2 5");
-		return;		
-}	
+		return;	
+		}		
 	
-		int f_catet = Integer.parseInt(args[0]);
-		int s_catet = Integer.parseInt(args[1]);
+		double f_catet = Double.parseDouble(args[0]);
+		double s_catet = Double.parseDouble(args[1]);
 
-		double gip = Math.sqrt(Math.pow(f_catet,2)+Math.pow(s_catet,2));
-		show("First");
-		System.out.println("Gipotenuza: " + gip);
+		Triangle triangle = new Triangle(f_catet,s_catet);
+		triangle.info();
 		
-		double f_angle = (Math.cos(f_catet/gip)/Math.PI)*180;
-		
-		System.out.println("Angles: "+90.0 +"  "+ Math.rint(f_angle) +" "+Math.rint(90 - f_angle) );
-		
-		System.out.println("Square: "+0.5*f_catet*s_catet);
-		System.out.println("Perimeter: "+(f_catet+s_catet+gip));
-		getch();
-	}
-
-	public static void show(String msg){
-		System.out.println("==========");
-		System.out.println(msg);
-		System.out.println("==========");
-	}
-
-	public static void getch(){
-		try{
-			System.in.read();
-		}catch (Exception e){}		
-	}
+}	
 }
