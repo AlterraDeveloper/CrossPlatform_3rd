9991a5f8db759d5686d1d054c37c2b2e4d4b01fc
Evgenii Kiselev <jkiselev2411@gmail.com>
2018-10-09 23:45:16 +0800

    practice 3 DONE

--- pract#3/MyStruct.java
+++ pract#3/MyStruct.java
@@ -6,12 +6,14 @@ class MyStruct {
     public static void main(String[] args) {
 
         show("First");
+        System.out.print("Enter size of array : ");
         Scanner scan = new Scanner(System.in);
         int[] arr = new int[scan.nextInt()];
         for (int i = 0; i < arr.length; i++)
             arr[i] = i + 10;
         int[] big_arr = new int[arr.length + 10];
         System.arraycopy(arr, 0, big_arr, 0, arr.length);
+        System.out.print("The array is increased by 10 elements : ");
         for (int i : big_arr)
             System.out.print(i + " ");
         System.out.println();
@@ -59,26 +61,47 @@ class MyStruct {
         TreeSet<String> treeSet = new TreeSet<String>(arrayList);
         Iterator<String> iterator = treeSet.iterator();
         System.out.println("Elements in TreeSet : ");
-        while(iterator.hasNext())
+        while (iterator.hasNext())
             System.out.print(iterator.next() + " ");
         getch();
 
         show("Sixth");
         System.out.println("Index of object \"Tim\" in ArrayList is : "
                 + (arrayList.indexOf("Tim") >= 0 ? arrayList.indexOf("Tim") : "object not found"));
-        System.out.println("Is HashMap contains value \"Bob\"? " + (hashMap.containsValue("Bob")==true ? "Yes" : "No"));
-        System.out.println("Is HashMap contains key \"35\"? " + (hashMap.containsKey(35)==true ? "Yes" : "No"));
+        System.out.println("Is HashMap contains value \"Bob\"? " + (hashMap.containsValue("Bob") == true ? "Yes" : "No"));
+        System.out.println("Is HashMap contains key \"35\"? " + (hashMap.containsKey(35) == true ? "Yes" : "No"));
         getch();
 
-         show("Seventh");
+        show("Seventh");
+        int searchKey = Math.abs(new Random().nextInt() % 10);
+        int keyIndex = Arrays.binarySearch(arr1, searchKey);
+        System.out.println("Index of " + searchKey + " in array of third task is : " +
+                (keyIndex >= 0 ? keyIndex : "element not found"));
+        getch();
 
-         getch();
+        show("Eighth");
+        System.out.println("Converting ArrayList to Array of objects...");
+        for (Object e : arrayList.toArray())
+            System.out.print(e + " ");
+        System.out.println();
+        getch();
 
-        // show("Eighth");
-        // getch();
-
-        // show("Ninth");
-        // getch();
+        show("Ninth");
+        System.out.println("Converting HashMap to Array...");
+        Object[] hashMapValuesArray = hashMap.values().toArray();
+        Object[] hashMapKeysArray = hashMap.keySet().toArray();
+        Object[] resultArray =
+                new Object[hashMapKeysArray.length + hashMapValuesArray.length];
+        for (int i = 0; i < resultArray.length; i++) {
+            if (i % 2 == 0) {
+                System.arraycopy(hashMapKeysArray, i%3 , resultArray, i, 1);
+                System.out.print(resultArray[i] + " ");
+            } else {
+                System.arraycopy(hashMapValuesArray, (i-1)%3 , resultArray, i, 1);
+                System.out.println(resultArray[i]);
+            }
+        }
+        getch();
     }
 
 
