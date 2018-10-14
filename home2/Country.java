940d8b6796daa2eb5a38568598abeb82437be919
Evgenii Kiselev <jkiselev2411@gmail.com>
2018-10-14 18:37:19 +0800

    commit

--- home2/Country.java
+++ home2/Country.java
@@ -1,15 +1,13 @@
-package home#2;
-
 public class Country{
 	private String name;
 	private String capital;
 	private double square;
 	private long population;
 
-	Country(){
+	public Country(){
 	}
 
-	Country(String name,String capital,double square,long population){
+	public Country(String name,String capital,double square,long population){
 		this.name = name;
 		this.capital = capital;
 		this.square = square;
@@ -30,6 +28,34 @@ public class Country{
 
 	public void setPopulation(long value){
 		population = value;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public String getCapital(){
+		return capital;
+	}
+
+	public double getSquare(){
+		return square;
+	}
+
+	public long getPopulation(){
+		return population;
+	}
+
+	public void show(){
+		System.out.println("Country name : " + name);
+		System.out.println("Country capital : " + capital);
+		System.out.println("Country square : " + square + " sq.km");
+		System.out.println("Country population : " + population + " people");
+	}
+
+	@Override
+	public String toString(){
+		return name + " " + capital + " " +square + "sq.km " + population + " people";	
 	}
 
 }
--- home2/Start.java
+++ home2/Start.java
@@ -1,7 +1,44 @@
-import home#2.Country;
-
 public class Start{
 	public static void main(String[] args){
-			Country country = new Country("Russia","Moscow",1615156.5848,149587452);
+
+		
+		Country[] world = new Country[5];
+		world[0] = new Country("Russia","Moscow",1615156.5848,149587452);
+		world[1] = new Country("Kyrgyzstan","Bishkek",12956.256,5421785);
+		world[2] = new Country("China","Beijing",1125489.156,2023256123);
+		world[3] = new Country("Brazil","Brazilia",8515767.345,210147125);
+		world[4] = new Country("India","New Delhi",3287263.15615,1324171354);	
+		
+		System.out.println("Country which name equals \"brazil\" : ");
+		for(Country c : world){
+			if(c.getName().equalsIgnoreCase("brazil")){
+				System.out.println("Object was found by name : " + c);			
+			}		
+		}
+		System.out.println();
+		
+		System.out.println("Country which capital is \"bishkek\" : ");
+		for(Country c : world){
+			if(c.getCapital().equalsIgnoreCase("bishkek")){
+				System.out.println("Object was found by capital : " + c);			
+			}		
+		}
+		System.out.println();
+
+		System.out.println("Countries which area more than 1,000,000.0 sq.km : ");
+		for(Country c : world){
+			if(c.getSquare() > 1000000.0){
+				System.out.println("Object was found by square : " + c);			
+			}		
+		}
+		System.out.println();
+
+		System.out.println("Countries which population more or equals 200,000,000 people : ");
+		for(Country c : world){
+			if(c.getPopulation() >= 200000000){
+				System.out.println("Object was found by population : " + c);			
+			}		
+		}
+		System.out.println();
 	}
 }
