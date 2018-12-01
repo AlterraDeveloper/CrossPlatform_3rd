fe4fb4d99b68a2aab7bd732611289fb7b8bf17a5
Evgenii Kiselev <jkiselev2411@gmail.com>
2018-12-02 00:17:16 +0600

    pract7 done 5/13

--- pract7/WorkWithFiles.java
+++ pract7/WorkWithFiles.java
@@ -1,28 +1,86 @@
 import java.io.*;
+import java.util.Date;
 
-public class WorkWithFiles{
+public class WorkWithFiles {
 
-public static void main(String[] args){
+ public static void main(String[] args) {
 
-File file = new File("D:\\Kiselev\\Test.txt");
 
-try{
-file = new File(file.getParent()+"\\myfile.txt");
+  show("first");
+  try {
+   File file = new File("Test.txt");
+   file.createNewFile();
+   File tmpFile = File.createTempFile("test", null, new File("."));
+   System.out.println("Successfully created files : " + file.getName() + " , " + tmpFile.getName());
+  } catch (IOException ioex) {
+   ioex.printStackTrace();
+  }
+  getch();
 
-file.createNewFile();
-for(int i=0;i<100000;i++){
-	for(int j=0;j<1000000;j++){}}
+  show("second");
+  try {
+   File file = new File("myfile.txt");
+   file.createNewFile();
+   System.out.println("Wait...7 seconds...");
+   try {
+    java.util.concurrent.TimeUnit.SECONDS.sleep(7);
+   } catch (InterruptedException iex) {
+    iex.printStackTrace();
+   }
+   if (file.renameTo(new File("yourfile.txt")))
+    System.out.println("Successfully renamed file myfile.txt to yourfile.txt");
+   else System.out.println("Renaming failed");
+   file = new File("folder");
+   file.mkdir();
+   if (file.renameTo(new File("folder-123")))
+    System.out.println("Successfully renamed directory folder to folder-123");
+   else System.out.println("Renaming failed");
+  } catch (IOException ioex) {
+   ioex.printStackTrace();
+  }
+  getch();
 
-file.renameTo(new File(file.getParent()+"\\newfile.txt"));
+  show("third");
+  File file = new File(".");
+  for (String str: file.list()) {
+   if (str.contains("txt") || str.contains("tmp") || new File(str).isDirectory()) {
+    if (new File(str).delete())
+     System.out.println("Successfully deleted : " + str);
+   }
+  }
+  getch();
 
-file = new File(file.getParent());
-System.out.println(file);
-file.renameTo(new File(file.getParent() + "\\Kiselev_EUgene");
-//File.createTempFile("test",null,new File(file.getParent()));
+  show("fourth");
+  file = new File("qwerty.txt");
+  try {
+   file.createNewFile();
+  } catch (IOException ioex) {
+   ioex.printStackTrace();
+  }
+  if (file.setReadOnly())
+   System.out.println("Successfully make file qwerty.txt readonly");
+  getch();
 
-}catch(IOException ioe){
-System.out.println("Smth go wrong!");
-}
+  show("fifth");
+  file = new File("qwerty.txt");
+  file.setLastModified(new Date(1998 - 1900, 10, 24).getTime());
+  System.out.println("Successfully change last modified time \"qwerty.txt\"");
+  getch();
 
-}
+ }
+
+ static void show(String s) {
+  System.out.println("---------------------------");
+  System.out.println("task " + s);
+  System.out.println("---------------------------");
+ }
+
+ static void getch() {
+  try {
+   System.in.read();
+  } catch (Exception e) {
+   e.printStackTrace();
+  }
+ }
+
 }
